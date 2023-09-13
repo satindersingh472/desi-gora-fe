780b648af1d54ca457a4ce579aa8ec0cd21de375
satinder <satindersingh472@gmail.com>
2023-09-13 17:52:03 -0600

    working-on-logo

---
+++ components/navbar/desktopNavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-app-bar app height="auto" class="white" flat >
+      <v-row>
+        <v-col>
+          <p class="mx-3 pa-2">
+            <span class="blue--text text--darken-4 text-h1 font-weight-bold mnr-3 "
+              >D</span
+            >
+            <span class="font-weight-regular text-h1"  >/</span>
+            <span class="deep-orange--text text-h1 font-weight-bold"
+              >G</span
+            >
+          </p>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
