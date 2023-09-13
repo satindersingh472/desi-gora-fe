49df8c303e29f5d7def81858ab3712f74877482e
satinder singh <satindersingh472@gmail.com>
2023-09-13 11:13:40 -0600

    image_cropping

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,12 @@ export default {
   pwa: {
     manifest: {
       lang: 'en',
+      title:'Rest-O-Pain',
+      icon:{
+        source:'/static/icons/logo.png',
+        sizes:[64, 120, 144, 152, 192, 384, 512],
+        purpose:'maskable'
+      }
     },
   },
 
