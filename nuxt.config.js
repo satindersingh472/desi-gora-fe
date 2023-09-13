8ce13207bd14d983c824ca41d4d2bf9b040e9250
satinder singh <satindersingh472@gmail.com>
2023-09-13 09:08:23 -0600

    added

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - desi-gora-fe',
-    title: 'desi-gora-fe',
+    titleTemplate: 'Desi Gora - %s',
+    title: 'Desi Gora',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
